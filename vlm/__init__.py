7c03b25f0809cd5d9d73b47d0c6f324f899ab57d
Rafael Gallardo <rgallardo.cs@outlook.com>
2024-09-29 23:28:47 -0600

    feat: implement Qwen2-VL class for a chatbot

--- vlm/__init__.py
+++ vlm/__init__.py
@@ -1,2 +1,2 @@
-from .hf_qwen2vl import HF_Qwen2_VL
+from .hf_qwen2vl import HF_Qwen2_Chatbot, HF_Qwen2_VLM
 from .vllm_qwen2vl import vLLM_Qwen2_VL
--- vlm/hf_qwen2vl.py
+++ vlm/hf_qwen2vl.py
@@ -1,15 +1,17 @@
 import logging
-from typing import Optional, Union
+from typing import Any, Dict, List, Optional, Union
 
 import torch
 from PIL import Image
 from qwen_vl_utils import process_vision_info
 from transformers import AutoProcessor, Qwen2VLForConditionalGeneration
 
+from .prompts import DEFAULT_CHAT_PROMPT, DEFAULT_NO_IMAGE_RESPONSE
+
 logging.basicConfig(level=logging.INFO)
 
 
-class HF_Qwen2_VL:
+class HF_Qwen2_VLM:
     def __init__(
         self,
         model_name: Optional[str] = "Qwen/Qwen2-VL-2B-Instruct",
@@ -59,35 +61,29 @@ class HF_Qwen2_VL:
         except Exception as e:
             raise Exception(f"Could not load processor for model {model_name}. Error: {e}")
 
-    def generate(self, image: Union[Image.Image, str], prompt: str) -> str:
+    def generate(self, messages: List[Dict[str, Any]]) -> str:
         """Generate text based on an image and a prompt.
 
         Args:
-            image (Union[Image.Image, str]): Image to use.
-            prompt (str): Prompt to use.
+            messages (List[Dict[str, Any]]): Messages, must be in the format of the Qwen2VL model.
+                e.g. messages = [
+                    {
+                        "role": "user",
+                        "content": [
+                            {
+                                "type": "image",
+                                "image": filepath, PIL image, URL,
+                            },
+                            {
+                                "type": "text",
+                                "text": "Describe this image.",
+                            },
+                        ],
+                    },
+                ]
         Returns:
             str: Generated text.
         """
-        messages = [
-            {
-                "role": "system",
-                "content": "You are a helpful assistant, based on a multimodal vision-language model",
-            },
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "image",
-                        "image": image,
-                    },
-                    {
-                        "type": "text",
-                        "text": prompt,
-                    },
-                ],
-            },
-        ]
-
         prompt = self.processor.apply_chat_template(
             messages,
             tokenize=False,
@@ -117,3 +113,95 @@ class HF_Qwen2_VL:
         )[0]
 
         return output_text
+
+
+class HF_Qwen2_Chatbot(HF_Qwen2_VLM):
+    def __init__(
+        self,
+        model_name: Optional[str] = "Qwen/Qwen2-VL-2B-Instruct",
+        min_pixels: Optional[int] = 256 * 28 * 28,
+        max_pixels: Optional[int] = 1280 * 28 * 28,
+        max_tokens: Optional[int] = 256,
+        force_image: Optional[bool] = False,
+    ) -> None:
+        super().__init__(model_name, min_pixels, max_pixels, max_tokens)
+
+        self.system_prompt = DEFAULT_CHAT_PROMPT
+        self.chat_context: List[Dict[str, Any]] = self._init_system_prompt()
+        self.is_image_set = False
+        self.force_image = force_image
+
+    def chat(
+        self,
+        prompt: str,
+        image: Optional[Union[str, Image.Image]] = None,
+        return_stream: Optional[bool] = True,
+    ) -> str:
+        """"""
+        # return a message indicating that the image is required
+        if self.force_image:
+            if image is None and not self.is_image_set:
+                return DEFAULT_NO_IMAGE_RESPONSE
+
+        # add the image to the context if it is not set
+        if image is not None and not self.is_image_set:
+            self._add_image_to_context(image, prompt)
+            self.is_image_set = True
+            return self.generate(messages=self.chat_context)
+
+        # generate the message
+        message = {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": prompt,
+                },
+            ],
+        }
+
+        # generate the response based on the prompt and the chat context
+        self.chat_context.append(message)
+        response = self.generate(messages=self.chat_context)
+
+        # adding the response to the chat context
+        self.add_assistant_message(response)
+
+        return response
+
+    def add_assistant_message(self, message: str) -> None:
+        """"""
+        message = {
+            "role": "system",
+            "content": message,
+        }
+        self.chat_context.append(message)
+
+    def _add_image_to_context(self, image: Union[str, Image.Image], prompt: str) -> None:
+        """"""
+        message = {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image",
+                    "image": image,
+                },
+                {
+                    "type": "text",
+                    "text": prompt,
+                },
+            ],
+        }
+        self.chat_context.append(message)
+
+    def _init_system_prompt(self):
+        return [
+            {
+                "role": "system",
+                "content": self.system_prompt,
+            },
+        ]
+
+    def reset_chat_context(self):
+        self.chat_context = self._init_system_prompt()
+        self.is_image_set = False
